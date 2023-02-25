e3f4f1c7f4784d4ab7d778ef9e0738d6f33cf08d
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-25 18:24:49 +0330

    change some codes in all file for code's check

--- test-react/src/components/NewExpense/NewExpense.js
+++ test-react/src/components/NewExpense/NewExpense.js
@@ -1,6 +1,7 @@
-import React from "react";
-import "./NewExpense.css";
-import ExpenseForm from "./ExpenseForm";
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
@@ -8,14 +9,12 @@ const NewExpense = (props) => {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-  props.onAddExpense(expenseData)
+    props.onAddExpense(expenseData);
   };
 
   return (
-    <div className="new-expense">
-      <form>
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-      </form>
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
